016ecb652ef7730fec2dbad22ee815a4b34733d9
Animasso Sidibe <asidibe017@gmail.com>
2023-07-21 05:59:44 +0200

    add: start the explore page

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,30 @@
-'use client';
-
+"use client";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import style from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+import { exploreWorlds } from "../constants";
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${style.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${style.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| The World" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Choose the world you want
+            <br className=" md:block hidden" /> to explore
+          </>
+        }
+        textStyles={"text-center"}
+      />
+    </motion.div>
   </section>
 );
 
