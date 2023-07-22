4fe107ababb341e11010b91d5e1c5bbadf344185
Animasso Sidibe <asidibe017@gmail.com>
2023-07-22 06:41:39 +0200

    add : finish explorer page startinggetStartedpage

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,7 +1,3 @@
-const StartSteps = () => (
-  <div>
-    start steps
-  </div>
-);
+const StartSteps = ({ number, text }) => <div>start steps</div>;
 
 export default StartSteps;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -5,27 +5,41 @@ import style from "../styles";
 import { staggerContainer } from "../utils/motion";
 import { TypingText, ExploreCard, TitleText } from "../components";
 import { exploreWorlds } from "../constants";
-const Explore = () => (
-  <section className={`${style.paddings}`} id="explore">
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${style.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      <TitleText
-        title={
-          <>
-            Choose the world you want
-            <br className=" md:block hidden" /> to explore
-          </>
-        }
-        textStyles={"text-center"}
-      />
-    </motion.div>
-  </section>
-);
+const Explore = () => {
+  const [active, setActive] = useState("world-2");
+  return (
+    <section className={`${style.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${style.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the world you want
+              <br className=" md:block hidden" /> to explore
+            </>
+          }
+          textStyles={"text-center"}
+        />
+        <div className=" mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, i) => (
+            <ExploreCard
+              key={world.id}
+              index={i}
+              {...world}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
